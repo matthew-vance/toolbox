e86fe8d3bba4eb4e38ceab1dff3e0cdec48786d0
Matthew Vance <mavance44@gmail.com>
2025-04-13 21:19:26 -0400

    create input component

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import type { HTMLInputAttributes } from "svelte/elements";
+  let { value = $bindable(), ...rest }: HTMLInputAttributes = $props();
+</script>
+
+<input bind:value {...rest} p="y-1 x-3" rounded="md" border="1 black" />
