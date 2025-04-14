2616b171a335d0775a942570dbdaac04d4bf67e6
Matthew Vance <mavance44@gmail.com>
2025-04-13 21:19:40 -0400

    create uuid generation page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import "virtual:uno.css";
+
+  let { children } = $props();
+</script>
+
+<svelte:head>
+  <title>Toolbox</title>
+  <meta name="description" content="A collection of useful tools." />
+</svelte:head>
+
+<div class="flex flex-row w-full">
+  <aside class="max-w-2xs p-4 flex flex-col gap-2">
+    <a href="/">Home</a>
+    <a href="/uuid" class="text-nowrap">UUID</a>
+  </aside>
+  <div class="w-full p-4">{@render children()}</div>
+</div>
---
+++ src/routes/uuid/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { v4 as uuidv4 } from "uuid";
+  import { Input } from "$lib/components";
+
+  let quantity = $state(1);
+  let uuids = $derived(Array.from({ length: quantity }, () => uuidv4()));
+</script>
+
+<main prose>
+  <h1>UUID Generator</h1>
+  <p>Generate a list of v4 UUIDs</p>
+  <label>Quantity: <Input type="number" bind:value={quantity} min="1" max="100" /></label>
+  {#each uuids as uuid}
+    <p class="not-prose">{uuid}</p>
+  {/each}
+</main>
