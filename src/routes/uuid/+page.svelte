7091f8d841c0cdfaf5e6aea828f47b8b006989fb
Matthew Vance <mavance44@gmail.com>
2025-04-16 19:50:12 -0400

    add uuid version select to generator

--- src/routes/uuid/+page.svelte
+++ src/routes/uuid/+page.svelte
@@ -1,15 +1,46 @@
 <script lang="ts">
-  import { v4 as uuidv4 } from "uuid";
+  import { v4, v7 } from "uuid";
   import { TextInput } from "$lib/components";
 
+  const uuidVersions = ["v4", "v7"] as const;
+  type UUIDVersion = (typeof uuidVersions)[number];
+  const generators = {
+    v4,
+    v7,
+  };
+
+  function generateUUIDs() {
+    return Array.from({ length: quantity }, () => generators[version]());
+  }
+
+  let version = $state<UUIDVersion>(uuidVersions[0]);
   let quantity = $state(1);
-  let uuids = $derived(Array.from({ length: quantity }, () => uuidv4()));
+  let uuids = $derived(generateUUIDs());
 </script>
 
 <main prose>
   <h1>UUID Generator</h1>
-  <p>Generate a list of v4 UUIDs</p>
-  <label>Quantity: <TextInput type="number" bind:value={quantity} min="1" max="100" /></label>
+  <p>Generate a list of UUIDs</p>
+  <div flex gap="4" items-end>
+    <div flex flex-col gap="4">
+      <label
+        >Version:
+        <select bind:value={version}>
+          {#each uuidVersions as ver}
+            <option value={ver}>{ver}</option>
+          {/each}
+        </select>
+      </label>
+      <label>Quantity: <TextInput type="number" bind:value={quantity} min="1" max="100" /></label>
+    </div>
+    <button
+      onclick={() => (uuids = generateUUIDs())}
+      border
+      p="y-1 x-3"
+      rounded-md
+      class="hover:bg-gray-100 hover:cursor-pointer">Regenerate</button
+    >
+  </div>
   {#each uuids as uuid}
     <p class="not-prose">{uuid}</p>
   {/each}
